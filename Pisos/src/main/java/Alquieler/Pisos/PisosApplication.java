b87a1b549f90f746fcf9725d2b953dec2707deb3
OUSSAMA BOLBAROUD <oussama.bolbaroud@alu.uclm.es>
2026-04-22 17:51:30 +0200

    initiliazar el proyecto

---
+++ Pisos/src/main/java/Alquieler/Pisos/PisosApplication.java
@@ -0,0 +1,13 @@
+package Alquieler.Pisos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PisosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PisosApplication.class, args);
+	}
+
+}
---
+++ Pisos/src/test/java/Alquieler/Pisos/PisosApplicationTests.java
@@ -0,0 +1,13 @@
+package Alquieler.Pisos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PisosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
